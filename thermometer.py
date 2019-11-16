e0a930c3fe192ec4c2861c1ccc7fd641406054d3
lugigann <57827194+lugigann@users.noreply.github.com>
2019-11-16 12:04:04 +0200

    Add commet logic Thermometer module

--- thermometer.py
+++ thermometer.py
@@ -3,14 +3,27 @@
 # set_temp
 # to_celsius
 # to_fahrenheit
+
 class Thermometer():
-    def __init__(self):
-        pass
-    def get_temperature(self):
-        pass
-    def to_celsius(self):
-        pass
-    def to_fahrenheit(self):
-        pass
-    
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def get_temperature(self, unit="C"):
+        if unit == "C":
+             return self.temperature
+        elif unit == "F":
+             return self.to_fahrenheit(self.temperature)
+
+    def set_temperture(self, temperature):
+        self.temperature = temperature
+
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) *5 / 9
+        return temperature
+
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 9 / 5 + 32
+        return temperature
+
+
 
